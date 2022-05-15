6f2bf1b6b14ffa4fad315ba05f50c64107b9f6de
agatabaczkiewicz <agata.baczkiewicz@gmail.com>
2022-05-15 13:44:39 +0200

    [PL-21]added share button with popover to display link

--- src/frontend/src/views/GameView.vue
+++ src/frontend/src/views/GameView.vue
@@ -1,23 +1,36 @@
 <template>
+  <div class="container">
+    <div class="row justify-content-start" >
+  <button type="button" class="btn btn-outline-secondary invite-btn col-8 align-start"
+          data-bs-toggle="popover" title="Copy invite link" data-bs-placement="bottom"
+          data-content-id="popover-27">
+    Invite to room</button>
+    </div>
+  <div style="display: none;" id="popover-27">
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">Link</span>
+      <input type="text" class="form-control"  aria-describedby="basic-addon1">
+    </div>
+  </div>
   <div class="room-table">
     <game-table ref="gameTableRef" :mainPlayerCard="this.mainPlayerCard" :isSelectionConfirmed="this.isSelectionConfirmed"
     />
     <main-player-space ref="mainPlayerSpaceRef" @clear="clearTableHandler" @selection="getSelection"/>
   </div>
-
+    </div>
 </template>
 
 <script>
-// import HeaderBar from "@/components/header-bar";
 import MainPlayerSpace from "@/components/MainPlayerSpace";
 import GameTable from "@/components/GameTable";
+import $ from 'jquery';
+import { Popover } from 'bootstrap/dist/js/bootstrap.esm.min.js'
 import {WebService} from "@/services/WebService";
 
 export default {
   name: "GameView",
   webService: new WebService(),
   components: {
-    // HeaderBar,
     MainPlayerSpace,
     GameTable
   },
@@ -26,6 +39,22 @@ export default {
       mainPlayerCard: '',
       isSelectionConfirmed: false
     }
+  },
+  mounted() {
+    let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    popoverTriggerList.forEach(popoverTriggerEl=> {
+        const popoverId = popoverTriggerEl.attributes['data-content-id'];
+        const contentEl = $(`#${popoverId.value}`).html();
+      let opts = {
+        content: contentEl,
+        html: true,
+        container: 'body',
+        sanitize  : false,
+        trigger: 'focus'
+              }
+        new Popover(popoverTriggerEl, opts);
+
+    })
   },
   methods:{
     getSelection($event){
@@ -44,6 +73,12 @@ export default {
 </script>
 
 <style scoped>
+
+.invite-btn{
+  width: 10rem;
+  margin-left: -8%;
+
+}
 .room {
   display: flex;
   flex-direction: column;
