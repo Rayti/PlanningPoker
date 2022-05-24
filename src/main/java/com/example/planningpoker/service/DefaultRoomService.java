e0083b6b619efa613340c37fa9f71a35b0ed1aff
Rayti <milosz.karlowicz@gmail.com>
2022-05-24 19:31:22 +0200

    PL-20 add logs and refactor service

--- src/main/java/com/example/planningpoker/service/DefaultRoomService.java
+++ src/main/java/com/example/planningpoker/service/DefaultRoomService.java
@@ -2,6 +2,7 @@ package com.example.planningpoker.service;
 
 import com.example.planningpoker.domain.Room;
 import com.example.planningpoker.domain.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
 
@@ -11,45 +12,47 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @ApplicationScope
 public class DefaultRoomService implements RoomService {
 
+    public static final String ROOM_DOES_NOT_EXIST_LOG = "Room {} does not exist";
     private List<Room> roomCache = Collections.synchronizedList(new ArrayList<>());
 
 
     @Override
     public boolean createRoom(String userName, String roomName) {
-        boolean notAlreadyExistsRoomName = roomCache.stream().noneMatch(room -> room.getRoomName().equals(roomName));
-        if(notAlreadyExistsRoomName){
+        if(getRoom(roomName) == null){
             Room room = new Room(roomName);
             User user = new User(userName, room);
             room.getUsers().add(user);
             roomCache.add(room);
+            log.info("Room {} and User {} created", roomName, userName);
             return true;
         }
+        log.info("Room {} already exists", roomName);
         return false;
     }
 
 
     @Override
     public boolean joinRoom(String userName, String roomName) {
-        Optional<Room> optionalRoom = roomCache.stream().filter(room -> room.getRoomName().equals(roomName)).findFirst();
+        Room room = getRoom(roomName);
 
-        if (optionalRoom.isEmpty()) {
+        if (room == null) {
+            log.info(ROOM_DOES_NOT_EXIST_LOG, roomName);
             return false;
         }
 
-        Room room = optionalRoom.get();
-
-        Optional<User> optionalUser = optionalRoom.get().getUsers().stream().filter(user -> user.getName().equals(userName)).findFirst();
-
-        if (optionalUser.isEmpty()) {
+         if (getUserFromRoom(room, userName) == null) {
             User user = new User(userName, room);
             room.getUsers().add(user);
+            log.info("User {} joined room {}", userName, roomName);
             return true;
         }
 
+        log.info("User {} already exists in room {}", userName, roomName);
         return false;
     }
 
@@ -58,11 +61,13 @@ public class DefaultRoomService implements RoomService {
     public boolean deleteRoom(String userName, String roomName) {
         Room room = getRoom(roomName);
         if (room == null) {
+            log.info(ROOM_DOES_NOT_EXIST_LOG, roomName);
             return false;
         }
         List<Room> roomsUpdated = roomCache.stream().filter(room1 -> !room1.getRoomName().equals(roomName)).collect(Collectors.toList());
         roomCache.clear();
         roomCache.addAll(roomsUpdated);
+        log.info("Room {} deleted", roomName);
         return true;
     }
 
@@ -71,17 +76,20 @@ public class DefaultRoomService implements RoomService {
     public boolean leaveRoom(String userName, String roomName) {
         Room room = getRoom(roomName);
         if (room == null) {
+            log.info(ROOM_DOES_NOT_EXIST_LOG, roomName);
             return false;
         }
 
         User user = getUserFromRoom(room, userName);
 
         if (user == null) {
+            log.info("User {} does not exist in room {}", userName, roomName);
             return false;
         }
 
         List<User> usersWithoutOneUser = room.getUsers().stream().filter(user1 -> !user1.getName().equals(userName)).collect(Collectors.toList());
         room.setUsers(usersWithoutOneUser);
+        log.info("User {} left room {}", userName, roomName);
         return true;
     }
 
