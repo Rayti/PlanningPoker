d2a389f5e92d8d8bb500a58e9a97a948fe688d24
Rayti <milosz.karlowicz@gmail.com>
2022-06-12 13:33:35 +0200

    PL-60 RegisterModal

---
+++ src/frontend/src/components/modals/RegisterModal.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="modal" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Registration panel</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" @click="closeModal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p class="text-start">Nickname:</p>
+          <div class="form-floating mb-3">
+            <input  class="form-control" id="registerNickname" v-model="registerNicknameInput" aria-label="Floating label select example" placeholder="Enter your nick">
+            <label for="registerNickname">Your nickname</label>
+          </div>
+          <p class="text-start">Password:</p>
+          <div class="form-floating mb-3">
+            <input  class="form-control" id="password" v-model="passwordInput" aria-label="Floating label select example" placeholder="Enter your password">
+            <label for="password">Your password</label>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" @click="registerNewAccount">Create new account</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: "RegisterModal",
+  inject: ['webService'],
+  data(){
+    return {
+      registerNicknameInput: "",
+      passwordInput: ""
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close-modal-event');
+    },
+    async registerNewAccount(){
+    //  TODO
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .modal {
+    display: block;
+  }
+</style>
--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,41 +1,42 @@
 <template>
 
 
-  <div :class="{'blur-content': displayModal && (createRoomModal || joinRoomModal)}">
+  <div :class="{'blur-content': displayModal}">
     <div class="container">
       <div class="row">
         <div class="col-10">
-          <img class="row text-margin" src="../assets/planningpoker.png">
-          <h3 class="row text-left text-margin mt-5 mb-4">To start planning</h3>
-          <div class="row justify-content-center">
-            <button col-4 type="button" class="btn btn-outline-primary btn-lg w-25 mb-4" @click="showModalRoom">
-              CREATE A NEW ROOM
-            </button>
-            <!--      <div class=" btn-toolbar  input-toolbar" role="toolbar" aria-label="Toolbar with button groups">-->
-            <!--        <div class="input-group">-->
-            <!--&lt;!&ndash;          <div class="input-group-text" id="btnGroupAddon">Name</div>&ndash;&gt;-->
-            <!--          <input type="text" v-model="nickInput" class="form-control" placeholder="Enter your nick"-->
-            <!--                 aria-label="Input group example" aria-describedby="btnGroupAddon">-->
-            <!--          <button type="button" v-on:click="goClick" class="btn btn-outline-success">Let's start</button>-->
-            <!--        </div>-->
-            <!--      </div>-->
-          </div>
-          <br>
-          <h3 class="row text-center text-margin mb-4 ">...or join existing one</h3>
-          <div class="row justify-content-center">
-            <div class=" btn-toolbar btn-lg mb-3 input-toolbar w-25" role="toolbar" aria-label="Toolbar with button groups">
-              <div class="input-group invite">
-                <input type="text" v-model="joinRoomInput" class="form-control " placeholder="Paste room name"
-                       aria-label="Input group example" aria-describedby="btnGroupAddon">
-                <button type="button" @click="joinRoom" class="btn btn-outline-primary">Join</button>
+          <div class="container">
+            <img class="row text-margin" src="../assets/planningpoker.png">
+            <h3 class="row text-left text-margin mt-5 mb-4">To start planning</h3>
+            <div class="row justify-content-center">
+              <button col-4 type="button" class="btn btn-outline-primary btn-lg w-25 mb-4" @click="showModalRoom">
+                CREATE A NEW ROOM
+              </button>
+              <!--      <div class=" btn-toolbar  input-toolbar" role="toolbar" aria-label="Toolbar with button groups">-->
+              <!--        <div class="input-group">-->
+              <!--&lt;!&ndash;          <div class="input-group-text" id="btnGroupAddon">Name</div>&ndash;&gt;-->
+              <!--          <input type="text" v-model="nickInput" class="form-control" placeholder="Enter your nick"-->
+              <!--                 aria-label="Input group example" aria-describedby="btnGroupAddon">-->
+              <!--          <button type="button" v-on:click="goClick" class="btn btn-outline-success">Let's start</button>-->
+              <!--        </div>-->
+              <!--      </div>-->
+            </div>
+            <br>
+            <h3 class="row text-center text-margin mb-4 ">...or join existing one</h3>
+            <div class="row justify-content-center">
+              <div class=" btn-toolbar btn-lg mb-3 input-toolbar w-25" role="toolbar" aria-label="Toolbar with button groups">
+                <div class="input-group invite">
+                  <input type="text" v-model="joinRoomInput" class="form-control " placeholder="Paste room name"
+                         aria-label="Input group example" aria-describedby="btnGroupAddon">
+                  <button type="button" @click="joinRoom" class="btn btn-outline-primary">Join</button>
+                </div>
               </div>
             </div>
           </div>
-
         </div>
         <div class="col-2">
           <div class="btn-group-vertical">
-            <button type="button" class="btn btn-outline-primary btn-lg"> Register</button>
+            <button type="button" class="btn btn-outline-primary btn-lg" @click="showRegisterModal"> Register</button>
             <button type="button" class="btn btn-outline-primary btn-lg"> Log in</button>
           </div>
         </div>
@@ -44,12 +45,14 @@
   </div>
   <create-room-modal v-if="createRoomModal" @close-modal-event="hideModal"></create-room-modal>
   <give-nick-modal v-if="joinRoomModal" :room-name="joinRoomInput" @close-modal-event="hideModal"></give-nick-modal>
+  <register-modal v-if="registerModal" @close-modal-event="hideModal"></register-modal>
 </template>
 
 <script>
 import {WebService} from "@/services/WebService";
 import GiveNickModal from "@/components/modals/GiveNickModal";
 import CreateRoomModal from "@/components/modals/CreateRoomModal";
+import RegisterModal from "@/components/modals/RegisterModal";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
@@ -60,6 +63,7 @@ export default {
   components: {
     GiveNickModal,
     CreateRoomModal,
+    RegisterModal
   },
   data() {
     return {
@@ -67,6 +71,7 @@ export default {
       displayModal: false,
       createRoomModal: false,
       joinRoomModal: false,
+      registerModal: false,
     }
   },
   methods: {
@@ -86,11 +91,15 @@ export default {
       this.displayModal = false;
       this.joinRoomModal = false;
       this.createRoomModal = false;
-
+      this.registerModal = false;
     },
     joinRoom(){
       this.displayModal = true;
       this.joinRoomModal = true;
+    },
+    showRegisterModal() {
+      this.displayModal = true;
+      this.registerModal = true;
     }
 
   }
