6978033002c2d8af8af998f71bcb3051774ae09f
Rayti <milosz.karlowicz@gmail.com>
2022-06-13 20:10:05 +0200

    PL-52 Personal data policy check

---
+++ src/frontend/src/views/PersonalDataPolicy.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <h3 class="title mb-4">Personal data policy:</h3>
+  </div>
+  <div class="container" style="text-align: left" >
+
+    <p >
+      In accordance with Article 13 para. 1 and para. 2 of the Regulation of the European Parliament and of the Council (EU) 2016/679 of 27 April 2016 on the protection of natural persons in regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46 /EC (Official Journal of the European Union L. 2016.119.1), General Data Protection Regulation, hereinafter referred to as "GDPR", I inform that:
+    </p>
+
+    <p>
+      1) The Administrator of your personal data is the British Council Foundation with its registered office at ul. Koszykowa 54, 00-675 Warsaw, registered at the District Court for the capital city of Warsaw in Warsaw, 12th Commercial Department of the National Register Courthouse under the number KRS 0000449621, REGON: 146529782, NIP: 7010369489.
+    </p>
+
+    <p>
+      2) The data administrator can be contacted via email: rodo@britishcouncil.pl or in writing at the address of the administrator's office with the note "Data protection".
+    </p>
+
+    <p>
+      3) Your personal data will be processed for the following purposes:
+    </p>
+
+    <p>
+      The purpose of personal data processing	Legal basis for processing
+      In order to comply with the terms of the participation agreement in an English language course
+      in order to execute and on the basis of a contract you are a party to
+    </p>
+
+    <p>
+      legal basis:  art. 6 para. 1 letter b GDPR <br>
+
+      Understanding your medical conditions that have or may have an impact on the learning process in order to properly adapt the teaching method and ensure your safety
+      processing is necessary to protect the vital interests of the data subject or another natural person <br>
+
+      legal basis:  art. 6 para. 1 letter d GDPR <br>
+
+      for execution and on the basis of a contract you are a party to <br>
+
+      legal basis:  art. 6 para. 1 letter b GDPR <br>
+
+      In order to fulfil legal obligations incumbent on the administrator of personal data resulting from legal provisions for tax and accounting purposes
+      fulfilling the legal obligations incumbent on the administrator <br>
+
+      legal basis:  art. 6 para. 1 letter c GDPR <br>
+
+
+
+      In order to assess your satisfaction and determine the quality of our service
+      execution of the legitimate interests of the administrator of personal data <br>
+
+      legal basis:  art. 6 para. 1 letter f GDPR <br>
+
+      For the purpose of marketing, promotion and sales of services
+      processing your personal data on the basis of execution of the legitimate interests of the administrator of personal data <br>
+
+      legal basis:  art. 6 para. 1 letter f GDPR <br>
+
+      The use of your data for the purposes of marketing the personal data administrator’s goods and services will take place on the basis of separate consents granted on the basis of art. 10 of the Act on the provision of electronic services and art. 172 of the Telecommunications Law <br>
+
+      For internal administrative purposes
+      execution of the legitimate interests of the administrator of personal data<br>
+
+      legal basis:  art. 6 para. 1 letter f GDPR<br>
+
+      In order to establish, investigate or defend against claims in connection with conducting dispute proceedings, proceedings before public authorities and other proceedings
+      execution of the legitimate interests of the administrator of personal data<br>
+
+      legal basis:  art. 6 para. 1 letter f GDPR<br>
+
+      In order to conduct video monitoring by the administrator, in accordance with the provisions of the labour law
+      execution of the legitimate interests of the administrator of personal data<br>
+
+      legal basis:  art. 6 para. 1 letter f GDPR
+    </p>
+    <p>
+      4) Your personal data may be transferred to: <br>
+
+      • entrepreneurs providing accounting and payroll, legal, insurance and IT services to the British Council Foundation;<br>
+
+      • national units of the British Council Foundation      <br>
+    </p>
+    <p>
+      5) Personal data will be processed for the following periods of time:
+
+      Your personal data will be processed for the period necessary for implementation of goals indicated in point 3.
+    </p>
+
+
+    <p>
+      6) The British Council Foundation ensures that all persons whose personal data is processed have appropriate rights resulting from the GDPR. Therefore, you have:<br>
+
+      1. the right to access personal data, including the right to obtain a copy of this data;<br>
+
+      2. the right to request correction (revision) of personal data - if the data is incorrect or incomplete<br>
+
+      3. the right to request the deletion of personal data (the so-called "right to be forgotten") - in the following events:<br>
+
+      a) the data is no longer necessary for the purposes for which it was collected or otherwise processed,<br>
+
+      b) the data subject has objected to the processing of the data<br>
+
+      c) the data subject has withdrawn the consent on which the processing is based and there is no other legal ground for processing,<br>
+
+      d) the data is processed unlawfully<br>
+
+      e) the data must be removed in order to comply with the legal obligation;<br>
+
+      4. the right to request a restriction to the processing of personal data - in the following events:<br>
+
+      a) the data subject questions the correctness of personal data,<br>
+
+      b) data processing is unlawful and the data subject opposes data deletion, demanding their restriction instead<br>
+
+      c) the administrator no longer needs data for his purposes, but the data subject needs them to identify, defend or pursue claims,<br>
+
+      d) the data subject has objected to the processing of the data - pending determination of whether the legitimate grounds on the part of the administrator override the grounds of objection;<br>
+
+      5. the right to transfer personal data – in the following events:<br>
+
+      a) the processing takes place on the basis of an agreement concluded with the data subject or on the basis of the consent expressed by such person and<br>
+
+      b) the processing is automated;<br>
+
+      6. the right to withdraw consent to the processing of personal data. To the extent you consented to the processing of personal data, you have the right to withdraw it. Withdrawal of consent does not affect the lawfulness of the processing of data which was made on the basis of consent before its withdrawal.
+    </p>
+    <p>
+      7) If you believe that the Data Administrator's processing violates the provisions of the GDPR, you have the right to file a complaint with the President of the Office for Personal Data Protection (ul. Stawki 2, 00-193 Warsaw).
+    </p>
+    <p>
+      8) Providing your personal data is a condition for entering into an agreement for participation in an English language course. Refusal to provide data will result in being unable to participate in a language course organized by the data administrator.
+    </p>
+  </div>
+  <div class="container mb-4">
+    <button class="btn btn-outline-primary btn-lg" onclick="window.history.go(-1)">Go Back</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "PersonalDataPolicy"
+}
+</script>
+<style scoped>
+</style>
